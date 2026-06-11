93668b9012f1390772783360908d3bc95bd4995d
Suguen Lim <guen8903@gmail.com>
2026-06-11 10:23:47 +0900

    chore: upgrade dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,14 @@ version = "1.0.0"
 
 dependencies {
     // common modules
-    implementation("com.factory.common:contract:1.0.1")
+    implementation("com.factory.common:contract:1.0.7")
     implementation("com.factory.common:kafka:1.0.7")       // CommonKafkaConsumer / EventDispatcher / EventHandler
     implementation("com.factory.common:inbox-jpa:1.0.7")   // @InboxProcessed
 
     // platform BOM
-    implementation(platform("com.factory:platform:1.0.5"))
-    annotationProcessor(platform("com.factory:platform:1.0.5"))
-    testAnnotationProcessor(platform("com.factory:platform:1.0.5"))
+    implementation(platform("com.factory:platform:1.0.7"))
+    annotationProcessor(platform("com.factory:platform:1.0.7"))
+    testAnnotationProcessor(platform("com.factory:platform:1.0.7"))
 
     // Spring Web
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -65,7 +65,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testRuntimeOnly("com.h2database:h2")
-    testImplementation("com.factory.common:contract:1.0.1")
+    testImplementation("com.factory.common:contract:1.0.7")
 
     // Dotenv
     implementation("me.paulschwarz:spring-dotenv:4.0.0")
